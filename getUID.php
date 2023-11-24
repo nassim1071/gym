b178a95351a562c8137864ac620fca6e71758b57
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-11-24 16:57:19 +0100

    UID 2

--- getUID.php
+++ getUID.php
@@ -1,5 +1,17 @@
 <?php
-	$UIDresult=$_POST["UIDresult"];
-	$Write="<?php $" . "UIDresult='" . $UIDresult . "'; " . "echo $" . "UIDresult;" . " ?>";
-	file_put_contents('UIDContainer.php',$Write);
+    // Get the XML data from the POST request
+    $xmlData = file_get_contents("php://input");
+
+    // Load the XML string
+    $xml = simplexml_load_string($xmlData);
+
+    // Extract UID from the XML
+    $UIDresult = (string)$xml->UIDresult;
+
+    // Process the UID as needed
+    // For example, you can store it in a file or a database
+    file_put_contents('UIDContainer.php', '<?php $UIDresult="' . $UIDresult . '"; echo $UIDresult; ?>');
+    
+    // Send a response (optional)
+    echo "Received UID: " . $UIDresult;
 ?>
--- manage_member.php
+++ manage_member.php
@@ -4,29 +4,36 @@
 
 
 
-<style> 
-  /* Add some styles to the controls div */
-  .controls {
-    margin-bottom: 20px; /* Add space below the controls */
-  }
+<style>
+	/* Add some styles to the controls div */
+	.controls {
+		margin-bottom: 20px;
+		/* Add space below the controls */
+	}
 
-  /* Style the textarea */
-  #getUID {
-    width: 150%; /* Take up 100% of the available width */
-	height: 55%;
-    padding: 10px; /* Add some padding for better visual appeal */
-    box-sizing: border-box; /* Include padding and border in the element's total width and height */
-    border: 1px solid #ccc; /* Add a border to the textarea */
-    border-radius: 5px; /* Add rounded corners */
-    font-size: 13px; /* Set the font size */
-  }
+	/* Style the textarea */
+	#getUID {
+		width: 150%;
+		/* Take up 100% of the available width */
+		height: 55%;
+		padding: 10px;
+		/* Add some padding for better visual appeal */
+		box-sizing: border-box;
+		/* Include padding and border in the element's total width and height */
+		border: 1px solid #ccc;
+		/* Add a border to the textarea */
+		border-radius: 5px;
+		/* Add rounded corners */
+		font-size: 13px;
+		/* Set the font size */
+	}
 </style>
 
 
 <script>
-	$(document).ready(function(){
-		 $("#getUID").load("UIDContainer.php");
-		setInterval(function() {
+	$(document).ready(function () {
+		$("#getUID").load("UIDContainer.php");
+		setInterval(function () {
 			$("#getUID").load("UIDContainer.php");
 		}, 500);
 	});
@@ -34,9 +41,9 @@
 
 
 <?php
-if(isset($_GET['id'])){
-	$qry = $conn->query("SELECT * FROM members where id=".$_GET['id'])->fetch_array();
-	foreach($qry as $k =>$v){
+if (isset($_GET['id'])) {
+	$qry = $conn->query("SELECT * FROM members where id=" . $_GET['id'])->fetch_array();
+	foreach ($qry as $k => $v) {
 		$$k = $v;
 	}
 }
@@ -45,7 +52,7 @@ if(isset($_GET['id'])){
 
 
 
-	//insert camera1 code here later
+//insert camera1 code here later
 
 ?>
 
@@ -54,135 +61,146 @@ if(isset($_GET['id'])){
 
 
 <div class="container-fluid">
-    <form action="" id="manage-member">
-        <div id="msg"></div>
-        <input type="hidden" name="id" value="<?php echo isset($_GET['id']) ? $_GET['id'] : '' ?>" class="form-control">
-        <div class="row form-group">
-            <div class="col-md-4">
-                <label class="control-label">ID No.</label>
-                <!-- Updated: Display UID from Arduino -->
-                <div id="uidDisplay">Please Scan your Card / Key Chain to display ID</div>
-            </div>
-        </div>
-    </form>
-
-    <script>
-        function fetchUID() {
-            // Use AJAX to fetch UID from the PHP container file
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    // Update the frontend with the received UID
-                    document.getElementById('uidDisplay').innerText = xhr.responseText;
-                }
-            };
-            xhr.open('GET', 'UIDContainer.php', true);
-            xhr.send();
-        }
-
-        // Fetch UID initially and set an interval to update regularly
-        fetchUID();
-        setInterval(fetchUID, 5000); // Update every 5 seconds
-    </script>
-		
-
-
-
-
-
+	<form action="" id="manage-member">
+		<div id="msg"></div>
+		<input type="hidden" name="id" value="<?php echo isset($_GET['id']) ? $_GET['id'] : '' ?>" class="form-control">
 		<div class="row form-group">
 			<div class="col-md-4">
-				<label class="control-label">Last Name</label>
-				<input type="text" name="lastname" class="form-control" value="<?php echo isset($lastname) ? $lastname:'' ?>" required>
-			</div>
-			<div class="col-md-4">
-				<label class="control-label">First Name</label>
-				<input type="text" name="firstname" class="form-control" value="<?php echo isset($firstname) ? $firstname:'' ?>" required>
-			</div>
-			<div class="col-md-4">
-				<label class="control-label">Middle Name</label>
-				<input type="text" name="middlename" class="form-control" value="<?php echo isset($middlename) ? $middlename:'' ?>">
+				<label class="control-label">ID No.</label>
+				<!-- Updated: Display UID from Arduino -->
+				<div id="uidDisplay">Please Scan your Card / Key Chain to display ID</div>
 			</div>
 		</div>
-		<div class="row form-group">
-			<div class="col-md-4">
-				<label class="control-label">Email</label>
-				<input type="email" name="email" class="form-control" value="<?php echo isset($email) ? $email:'' ?>" required>
-			</div>
-			<div class="col-md-4">
-				<label class="control-label">Contact #</label>
-				<input type="text" name="contact" class="form-control" value="<?php echo isset($contact) ? $contact:'' ?>" required>
-			</div>
-			<div class="col-md-4">
-				<label class="control-label">Gender</label>
-				<select name="gender" required="" class="custom-select" id="">
-					<option <?php echo isset($gender) && $gender == 'Male' ? 'selected' : '' ?>>Male</option>
-					<option <?php echo isset($gender) && $gender == 'Female' ? 'selected' : '' ?>>Female</option>
-				</select>
-			</div>
+	</form>
+
+	<script>
+		function fetchUID() {
+			// Use AJAX to fetch UID from the PHP container file
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					// Update the frontend with the received UID
+					document.getElementById('uidDisplay').innerText = xhr.responseText;
+				}
+			};
+			xhr.open('GET', 'UIDContainer.php', true);
+			xhr.send();
+		}
+
+		// Fetch UID initially and set an interval to update regularly
+		fetchUID();
+		setInterval(fetchUID, 5000); // Update every 5 seconds
+	</script>
+
+
+
+
+
+
+	<div class="row form-group">
+		<div class="col-md-4">
+			<label class="control-label">Last Name</label>
+			<input type="text" name="lastname" class="form-control"
+				value="<?php echo isset($lastname) ? $lastname : '' ?>" required>
 		</div>
-		<div class="row form-group">
-			<div class="col-md-12">
-				<label class="control-label">Address</label>
-				<textarea name="address" class="form-control"><?php echo isset($address) ? $address : '' ?></textarea>
-			</div>
+		<div class="col-md-4">
+			<label class="control-label">First Name</label>
+			<input type="text" name="firstname" class="form-control"
+				value="<?php echo isset($firstname) ? $firstname : '' ?>" required>
 		</div>
-		<div class="row form-group">
-			<div class="col-md-4">
-				<label class="control-label">Plan</label>
-				<select name="plan_id" required="required" class="custom-select select2" id="">
-					<option value=""></option>
-					<?php
-						$qry = $conn->query("SELECT * FROM plans order by plan asc");
-						while($row= $qry->fetch_assoc()):
+		<div class="col-md-4">
+			<label class="control-label">Middle Name</label>
+			<input type="text" name="middlename" class="form-control"
+				value="<?php echo isset($middlename) ? $middlename : '' ?>">
+		</div>
+	</div>
+	<div class="row form-group">
+		<div class="col-md-4">
+			<label class="control-label">Email</label>
+			<input type="email" name="email" class="form-control" value="<?php echo isset($email) ? $email : '' ?>"
+				required>
+		</div>
+		<div class="col-md-4">
+			<label class="control-label">Contact #</label>
+			<input type="text" name="contact" class="form-control" value="<?php echo isset($contact) ? $contact : '' ?>"
+				required>
+		</div>
+		<div class="col-md-4">
+			<label class="control-label">Gender</label>
+			<select name="gender" required="" class="custom-select" id="">
+				<option <?php echo isset($gender) && $gender == 'Male' ? 'selected' : '' ?>>Male</option>
+				<option <?php echo isset($gender) && $gender == 'Female' ? 'selected' : '' ?>>Female</option>
+			</select>
+		</div>
+	</div>
+	<div class="row form-group">
+		<div class="col-md-12">
+			<label class="control-label">Address</label>
+			<textarea name="address" class="form-control"><?php echo isset($address) ? $address : '' ?></textarea>
+		</div>
+	</div>
+	<div class="row form-group">
+		<div class="col-md-4">
+			<label class="control-label">Plan</label>
+			<select name="plan_id" required="required" class="custom-select select2" id="">
+				<option value=""></option>
+				<?php
+				$qry = $conn->query("SELECT * FROM plans order by plan asc");
+				while ($row = $qry->fetch_assoc()):
 					?>
-					<option value="<?php echo $row['id'] ?>" <?php echo isset($plan_id) && $plan_id == $row['id'] ? 'selected' : '' ?>><?php echo ucwords($row['plan']) ?></option>
-					<?php endwhile; ?>
-				</select>
-			</div>
-			<div class="col-md-4">
-				<label class="control-label">Package</label>
-				<select name="package_id" required="required" class="custom-select select2" id="">
-					<option value=""></option>
-					<?php
-						$qry = $conn->query("SELECT * FROM packages order by package asc");
-						while($row= $qry->fetch_assoc()):
-					?>
-					<option value="<?php echo $row['id'] ?>" <?php echo isset($package_id) && $package_id == $row['id'] ? 'selected' : '' ?>><?php echo ucwords($row['package']) ?></option>
-					<?php endwhile; ?>
-				</select>
-			</div>
-			<div class="col-md-4">
-				<label class="control-label">Trainer</label>
-				<select name="trainer_id" class="custom-select select2" id="">
-					<option value=""></option>
-					<?php
-						$qry = $conn->query("SELECT * FROM trainers order by name asc");
-						while($row= $qry->fetch_assoc()):
-					?>
-					<option value="<?php echo $row['id'] ?>" <?php echo isset($trainer_id) && $trainer_id == $row['id'] ? 'selected' : '' ?>><?php echo ucwords($row['name']) ?></option>
-					<?php endwhile; ?>
-				</select>
-			</div>
+					<option value="<?php echo $row['id'] ?>" <?php echo isset($plan_id) && $plan_id == $row['id'] ? 'selected' : '' ?>>
+						<?php echo ucwords($row['plan']) ?>
+					</option>
+				<?php endwhile; ?>
+			</select>
 		</div>
+		<div class="col-md-4">
+			<label class="control-label">Package</label>
+			<select name="package_id" required="required" class="custom-select select2" id="">
+				<option value=""></option>
+				<?php
+				$qry = $conn->query("SELECT * FROM packages order by package asc");
+				while ($row = $qry->fetch_assoc()):
+					?>
+					<option value="<?php echo $row['id'] ?>" <?php echo isset($package_id) && $package_id == $row['id'] ? 'selected' : '' ?>>
+						<?php echo ucwords($row['package']) ?>
+					</option>
+				<?php endwhile; ?>
+			</select>
+		</div>
+		<div class="col-md-4">
+			<label class="control-label">Trainer</label>
+			<select name="trainer_id" class="custom-select select2" id="">
+				<option value=""></option>
+				<?php
+				$qry = $conn->query("SELECT * FROM trainers order by name asc");
+				while ($row = $qry->fetch_assoc()):
+					?>
+					<option value="<?php echo $row['id'] ?>" <?php echo isset($trainer_id) && $trainer_id == $row['id'] ? 'selected' : '' ?>>
+						<?php echo ucwords($row['name']) ?>
+					</option>
+				<?php endwhile; ?>
+			</select>
+		</div>
+	</div>
 	</form>
 </div>
 
 <script>
-	$('#manage-member').submit(function(e){
+	$('#manage-member').submit(function (e) {
 		e.preventDefault()
 		start_load()
 		$.ajax({
-			url:'ajax.php?action=save_member',
-			method:'POST',
-			data:$(this).serialize(),
-			success:function(resp){
-				if(resp == 1){
-					alert_toast("Data successfully saved.",'success')
-					setTimeout(function(){
+			url: 'ajax.php?action=save_member',
+			method: 'POST',
+			data: $(this).serialize(),
+			success: function (resp) {
+				if (resp == 1) {
+					alert_toast("Data successfully saved.", 'success')
+					setTimeout(function () {
 						location.reload()
-					},1000)
-				}else if(resp == 2){
+					}, 1000)
+				} else if (resp == 2) {
 					$('#msg').html('<div class="alert alert-danger">ID No already existed.</div>')
 					end_load();
 				}
@@ -191,8 +209,8 @@ if(isset($_GET['id'])){
 	})
 
 
-	
 
 
 
-    </script>
+
+</script>
